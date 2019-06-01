8a86b58c97c789ee90468c4f58644e101bda9926
Sidharth Satapathy <sidharth.satapathy9@gmail.com>
2019-06-02 01:05:00 +0530

    Basic website structure added.

---
+++ src/Components/LogoBox.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class LogoBox extends React.Component {
+    render() {
+        return <div className="logoBox">
+            <img src="../assets/logo_transparent.png" alt="Logo" class="logo"/>
+        </div>
+    }
+}
