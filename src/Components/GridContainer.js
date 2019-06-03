377c5ec75d1cd02837e217501f926e1222e32510
Sidharth Satapathy <sidharth.satapathy9@gmail.com>
2019-06-03 21:02:19 +0530

    Skeleton structure added.

--- src/Components/GridContainer.js
+++ src/Components/GridContainer.js
@@ -5,6 +5,7 @@ class GridContainer extends React.Component {
 
     getItems = () => {
         const { gifsData } = this.props
+        const skeletonArray = [0,1,2,3,4,5,6,7,8,9]
         return gifsData
                 ? gifsData.map(
                     (gif, index) => {
@@ -17,7 +18,11 @@ class GridContainer extends React.Component {
                             </div>
                     }
                 )
-                : []
+                : skeletonArray.map( (data, index) => {
+                    return <div className={`gallery__item gallery__item--${index+1}`}>
+                            <div className="skeleton"></div>
+                        </div>
+                } )
     }
 
     render() {
