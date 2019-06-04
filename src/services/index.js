c8fc35399f3c08389fbcf3cf318b4f3250153550
Sidharth Satapathy <sidharth.satapathy9@gmail.com>
2019-06-04 11:52:21 +0530

    Code refractor.

--- src/services/index.js
+++ src/services/index.js
@@ -8,11 +8,13 @@ const createClient = (baseUrl) => {
 
 const ApiKey = '2VfSumXH0e5ZETPZ2asCDsYPsEQgBD9D'
 
+const gifLimit = 500
+
 const GiphyEndPoint = createClient('https://api.giphy.com')
 
 const getGifs = (searchTerm) => 
     GiphyEndPoint
-    .get(`/v1/gifs/search?api_key=${ApiKey}&&q=${searchTerm}&&limit=500`)
+    .get(`/v1/gifs/search?api_key=${ApiKey}&&q=${searchTerm}&&limit=${gifLimit}`)
     .then(response => response.data.data)
 
 export {
